054a6d1d2e703fe0356a3c5a9c725230fe084e2a
Adam Crews <adam@pathdefender.com>
2014-06-19 10:35:26 -0700

    Fix templates

--- manifests/https.pp
+++ manifests/https.pp
@@ -6,12 +6,13 @@ define pound::https (
   $ensure           = 'present',
   $target           = '/etc/pound.conf',
   $custom_template  = undef,
+  $ciphers          = 'ECDH+AESGCM:DH+AESGCM:ECDH+AES256:DH+AES256:ECDH+AES128:DH+AES:ECDH+3DES:DH+3DES:RSA+AESGCM:RSA+AES:RSA+3DES:!aNULL:!MD5:!DSS;'
 ) { 
   
   if $custom_template { 
     $content = template($custom_template)
   } else {
-    # This template uses $cert, $address, $port, $backend
+    # This template uses $cert, $address, $port, $backend, $ciphers
     $contnet = template("${module_name}/https.erb")
   }
     
--- manifests/params.pp
+++ manifests/params.pp
@@ -5,7 +5,6 @@ class pound::params {
   $service_manage = true
   $user           = 'nobody'
   $group          = 'nogroup'
-  $ciphers        = 'ECDH+AESGCM:DH+AESGCM:ECDH+AES256:DH+AES256:ECDH+AES128:DH+AES:ECDH+3DES:DH+3DES:RSA+AESGCM:RSA+AES:RSA+3DES:!aNULL:!MD5:!DSS;'
 
   case $::osfamily { 
     'RedHat': { 
